7f72b524493fa8617d82561d34d207e4c2acd560
niq414 <laniqua.webb@aim.com>
2015-02-11 20:14:18 -0500

    Added
    calculations

--- Webb_Laniqua_Epressions_Assignment/js/script.js
+++ Webb_Laniqua_Epressions_Assignment/js/script.js
@@ -10,3 +10,7 @@ var thirdGraders = prompt("How many Third Graders will be taking the test?");
 var fourthGraders = prompt("How many Fourth Graders will be taking the test? ");
 var fifthGraders = prompt("How many Fifth Graders will be taking the test?");
 
+var totalPencils = thirdGraders * 2 + fourthGraders * 3 + fifthGraders * 4;
+
+console.log("You will need " + totalPencils + " for testing.");
+alert("You will need " + totalPencils + " for testing.")
