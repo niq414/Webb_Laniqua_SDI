96bba19610ddd13e1beb85844153e3fc3cd4a04b
niq414 <laniqua.webb@aim.com>
2015-02-11 20:08:26 -0500

    Givens
    entered given information and prompt

---
+++ Webb_Laniqua_Epressions_Assignment/js/script.js
@@ -0,0 +1,12 @@
+//Laniqua Webb February 11, 2015 Expressions Assignment
+
+//This is calculator that will calculate the amount of number 2 pencils a school will need for standardized testing based on the amount of students in each grade level
+
+var thirdGraders = 2;
+var fourthGraders = 3;
+var fifthGraders = 4;
+
+var thirdGraders = prompt("How many Third Graders will be taking the test?");
+var fourthGraders = prompt("How many Fourth Graders will be taking the test? ");
+var fifthGraders = prompt("How many Fifth Graders will be taking the test?");
+
