960f9472947b4091d7d19ee41b5e14716946d7ee
niq414 <laniqua.webb@aim.com>
2015-02-19 22:41:45 -0500

    Variables
    Created Variables

---
+++ Webb_Laniqua_Conditionals_Assignments/js/script.js
@@ -0,0 +1,10 @@
+//Laniqua Webb - February 19th 2015 - Conditionals Assignment
+
+//This is a calculator that will calculate the minimum amount of points a rewards card member will need in order to receive a free gift
+
+
+var handBags = 5;
+var personalAccessories = 7;
+var clothingArticles = 10;
+var pointTotal = handBags * 5 + personalAccessories * 7 + clothingArticles * 10;
+
