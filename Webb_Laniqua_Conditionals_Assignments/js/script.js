2bed1bb7f3f7636f4d8e8db8aec598b309866264
niq414 <laniqua.webb@aim.com>
2015-02-19 23:10:32 -0500

    Shoes
    calculated shoes added

--- Webb_Laniqua_Conditionals_Assignments/js/script.js
+++ Webb_Laniqua_Conditionals_Assignments/js/script.js
@@ -1,10 +1,33 @@
 //Laniqua Webb - February 19th 2015 - Conditionals Assignment
 
 //This is a calculator that will calculate the minimum amount of points a rewards card member will need in order to receive a free gift
-
+//for the end of the year giveaway
+//This calculator will determine the amounts of points the card member will need to get during the sales period by calculating the
+//amount of points earned so far this year.
 
 var handBags = 5;
 var personalAccessories = 7;
 var clothingArticles = 10;
-var pointTotal = handBags * 5 + personalAccessories * 7 + clothingArticles * 10;
+var miscellaneousItems = 2;
 
+var pointTotal = handBags * 5 + personalAccessories * 7 + clothingArticles * 10 + miscellaneousItems *2;
+
+var handBags = prompt("How many handbags were purchased?");
+var personalAccessories = prompt("How many personal accessories were purchased?");
+var clothingArticles = prompt("How many clothing articles were purchased?");
+var miscellaneousItems = prompt("Were any miscellaneous items purchased?");
+
+//Determine how many pairs of shoes were purchased each quarter
+
+var shoesPurchased = [0,1,2,3];
+
+var shoesPurchased0 = prompt("How many shoes were purchased in the first quarter of the year?");
+var shoesPurchased1 = prompt("How many shoes were purchased in the second quarter of the year?");
+var shoesPurchased2 = prompt("How many shoes were purchased in the third quarter of the year?");
+var shoesPurchased3 = prompt("How many shoes were purchased in the fourth quarter of the year?");
+
+var shoeTotal = shoesPurchased[0] + shoesPurchased[1] + shoesPurchased[2] + shoesPurchased[3];
+
+var shoeAverage = shoeTotal/4;
+
+console.log("You have purchased a total of " +shoeTotal+ " for the year. That is an average of " +shoeAverage+ " shoes each quarter!")
